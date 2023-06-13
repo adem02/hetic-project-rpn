3f420fb3ceb1a4308cd086c2e9b0add931baa20e
trak-dev <yann.picaud@gmail.com>
2023-06-13 16:27:17 +0200

    single responsability way

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,37 +1,37 @@
-import { evalRPN } from './index';
+import { evaluateRpn } from './index';
 
 describe('Succes cases', () => {
 
   it('should return 0 for ["0"]', () => {
-    expect(evalRPN(["0"])).toEqual(0);
+    expect(evaluateRpn(["0"])).toEqual(0);
   });
 
   it('should return 5 for 2 + 3', () => {
-    expect(evalRPN(["2", "3", "+"])).toEqual(5);
+    expect(evaluateRpn(["2", "3", "+"])).toEqual(5);
   });
 
   it('should return 6 for 2 * 3', () => {
-    expect(evalRPN(["2", "3", "*"])).toEqual(6);
+    expect(evaluateRpn(["2", "3", "*"])).toEqual(6);
   });
 
   it('should return 1 for 3 - 2', () => {
-    expect(evalRPN(["3", "2", "-"])).toEqual(1);
+    expect(evaluateRpn(["3", "2", "-"])).toEqual(1);
   });
 
   it('should return 1 for 3 / 2', () => {
-    expect(evalRPN(["3", "2", "/"])).toEqual(1);
+    expect(evaluateRpn(["3", "2", "/"])).toEqual(1);
   });
 
   it('should return 1 for 3 % 2', () => {
-    expect(evalRPN(["3", "2", "MOD"])).toEqual(1);
+    expect(evaluateRpn(["3", "2", "MOD"])).toEqual(1);
   });
 
   it('should return 1 for 3 + (-2)', () => {
-    expect(evalRPN(["3", "2", "NEGATE", "+"])).toEqual(1);
+    expect(evaluateRpn(["3", "2", "NEGATE", "+"])).toEqual(1);
   });
 
   it('should solve complex expression', () => {
-    expect(evalRPN(["5", "1", "2", "+", "4", "*", "+", "3", "-"])).toEqual(14);
+    expect(evaluateRpn(["5", "1", "2", "+", "4", "*", "+", "3", "-"])).toEqual(14);
   });
 
 });
@@ -39,31 +39,31 @@ describe('Succes cases', () => {
 describe('Error cases', () => {
 
   it('should throw an error if the user inputs a negative number', () => {
-    expect(() => evalRPN(["-1", "2", "+"])).toThrowError('Invalid RPN expression');
+    expect(() => evaluateRpn(["-1", "2", "+"])).toThrowError('Invalid RPN expression');
   });
 
   it('should throw an error if the user inputs an invalid expression', () => {
-    expect(() => evalRPN(["1", "10", "11", "+"])).toThrowError('Invalid RPN expression');
+    expect(() => evaluateRpn(["1", "10", "11", "+"])).toThrowError('Invalid RPN expression');
   });
 
   it('should throw an error if the user inputs an invalid expression', () => {
-    expect(() => evalRPN(["1", "+", "11", "+"])).toThrowError('Invalid RPN expression');
+    expect(() => evaluateRpn(["1", "+", "11", "+"])).toThrowError('Invalid RPN expression');
   });
 
   it('should throw error if there is division by zero', () => {
-    expect(() => evalRPN(["10", "0", "/"])).toThrowError('Division by zero');
+    expect(() => evaluateRpn(["10", "0", "/"])).toThrowError('Division by zero');
   });
 
   it('should throw error if there is no operand to calculate', () => {
-    expect(() => evalRPN(["10", "0", "5"])).toThrowError('Invalid RPN expression');
+    expect(() => evaluateRpn(["10", "0", "5"])).toThrowError('Invalid RPN expression');
   });
 
   it('should throw an error if the operator is not valid', () => {
-    expect(() => evalRPN(["10", "5", "♥"])).toThrowError('Invalid RPN expression');
+    expect(() => evaluateRpn(["10", "5", "♥"])).toThrowError('Invalid RPN expression');
   });
 
   it('should throw an error if there is no operand to calculate', () => {
-    expect(() => evalRPN(['+'])).toThrowError('Invalid RPN expression');
+    expect(() => evaluateRpn(['+'])).toThrowError('Invalid RPN expression');
   });
 
 }); 
--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,35 @@ function isValidOperand(token: string): boolean {
     return !isNaN(Number(token)) && Number(token) >= 0;
 }
 
-export function evalRPN(tokens: string[]): number {
+function evaluateOperator(token: string, stack: number[]): void {
+    // if token is NEGATE, pop the last operand from stack and continue to next iteration
+    if (token === 'NEGATE') {
+        const firstOperand = stack.pop();
+        stack.push(operatorSets[token](firstOperand, 0));
+    } else {
+        // pop the last two operands from stack
+        const secondOperand = stack.pop();
+        const firstOperand = stack.pop();
+        // throw error if there is division by zero
+        if (token === '/' && secondOperand === 0) throw new Error('Division by zero');
+
+        // throw error if there is no operand to calculate
+        if (firstOperand == null || secondOperand == null) throw new Error('Invalid RPN expression');
+
+        // push the result back to stack
+        stack.push(operatorSets[token](firstOperand, secondOperand));
+    }
+}
+
+function evaluateOperand(token: string, stack: number[]): void {
+    // if token is not an operator, push it to stack
+    if (!isValidOperand(token)) throw new Error('Invalid RPN expression ' + token);
+
+    // push the number to stack
+    stack.push(Number(token));
+}
+
+export function evaluateRpn(tokens: string[]): number {
 
     // initialising empty stack to calculate
     const stack: number[] = [];
@@ -30,32 +58,9 @@ export function evalRPN(tokens: string[]): number {
 
         // if token is an operator, pop the last two operands from stack and calculate
         if (isValidOperator(token)) {
-
-            // if token is NEGATE, pop the last operand from stack and continue to next iteration
-            if (token === "NEGATE") {
-                const firstOperand = stack.pop();
-                stack.push(operatorSets[token](firstOperand, 0));
-                continue;
-            }
-
-            // pop the last two operands from stack
-            const secondOperand = stack.pop();
-            const firstOperand = stack.pop();
-
-            // throw error if there is division by zero
-            if (token === '/' && secondOperand === 0) throw new Error('Division by zero');
-
-            // throw error if there is no operand to calculate
-            if (firstOperand == null || secondOperand == null) throw new Error('Invalid RPN expression');
-
-            // push the result back to stack
-            stack.push(operatorSets[token](firstOperand, secondOperand));
+            evaluateOperator(token, stack);
         } else {
-            // if token is not an operator, push it to stack
-            if (!isValidOperand(token)) throw new Error('Invalid RPN expression ' + token);
-
-            // push the number to stack
-            stack.push(Number(token));
+            evaluateOperand(token, stack);
         }
     }
 
@@ -64,4 +69,4 @@ export function evalRPN(tokens: string[]): number {
     return stack[0];
 };
 
-console.log(evalRPN(["10", "1", "+"]));
+console.log(evaluateRpn(["10", "NEGATE", "NEGATE", "NEGATE", "NEGATE", "1", "2", "+", "4", "*", "+", "3", "-"]));
